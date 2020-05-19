d8a819c75f3123d0c4b464c09c2b066597bb3a69
srishtirai <srishticrai@gmail.com>
2020-05-19 20:14:01 +0530

    Company names

--- api/api.py
+++ api/api.py
@@ -23,22 +23,32 @@ app = Flask(__name__)
 @app.route('/')
 def index(): 
 	files=os.listdir("C:/Users/RAI/Desktop/Final Project/Dataset")
-	return render_template("index.html",company=files)
+	data=[]
+	l=len(files)
+	for i in range(0,l):
+		file=files[i][:-4]
+		data.append (pd.read_csv("C:/Users/RAI/Desktop/Final Project/Dataset/"+file+".csv").tail(1))
+	return render_template("index.html",len=len(files),files=files,data=data)
 
 @app.route("/result",methods=["POST"])
 def result():
 	name=request.form.get("name")
-	df = pd.read_csv("C:/Users/RAI/Desktop/Final Project/Dataset/"+name+".csv")
-	df.tail(10)
-	data = df[['Date','Open','High','Low','Close','Volume','VWAP']]
-	df_vwap = df[['Date','VWAP']]
-	df_vwap['Date'] = df_vwap['Date'].apply(pd.to_datetime)
-	df_vwap.tail(10)
-	name=df['Symbol'][1]
-	plt.figure(figsize=(16,8)) 
-	plt.plot(df_vwap['VWAP'], label='VWAP') 
-	plt.title('VWAP visualization for the company : '+name) 
-	plt.xlabel("Time(year)") 
-	plt.ylabel("Volume Weighted Average Price") 
-	plt.legend(loc='best')
-	return render_template("result.html",name=plt.plot(df_vwap['VWAP'], label='VWAP') ) 
+	name=name.upper()
+	companys=os.listdir("C:/Users/RAI/Desktop/Final Project/Dataset")
+	if(name+".csv" in companys):
+		df = pd.read_csv("C:/Users/RAI/Desktop/Final Project/Dataset/"+name+".csv")
+		msg=name
+		data = df[['Date','Open','High','Low','Close','Volume','VWAP']]
+		df_vwap = df[['Date','VWAP']]
+		df_vwap['Date'] = df_vwap['Date'].apply(pd.to_datetime)
+		name=df['Symbol'][1]
+		plt.figure(figsize=(16,8)) 
+		plt.plot(df_vwap['VWAP'], label='VWAP') 
+		plt.title('VWAP visualization for the company : '+name) 
+		plt.xlabel("Time(year)") 
+		plt.ylabel("Volume Weighted Average Price") 
+		plt.legend(loc='best')
+		return render_template("result.html",name=plt.plot(df_vwap['VWAP'], label='VWAP'),msg=msg ) 
+	else:
+		msg="The provided company data is not available"
+		return render_template("notavail.html",msg=msg)
